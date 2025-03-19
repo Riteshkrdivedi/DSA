7b606de6f0bdbc171214d580d2b022f0e4033cb2
Ritesh <riteshkd2k20@gmail.com>
2025-03-19 21:38:43 +0530

    ...

--- sorting_Algorithms.cpp
+++ sorting_Algorithms.cpp
@@ -68,6 +68,7 @@ void insertion_sort(int arr[], int n)
         }
     }
 }
+
 void print_array(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -75,6 +76,46 @@ void print_array(int arr[], int n)
         cout << arr[i] << " ";
     }
     cout << endl;
+}
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int leftArr[n1], rightArr[n2];
+
+    for (int i = 0; i < n1; i++)
+        leftArr[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        rightArr[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2)
+    {
+        if (leftArr[i] <= rightArr[j])
+            arr[k++] = leftArr[i++];
+        else
+            arr[k++] = rightArr[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = leftArr[i++];
+
+    while (j < n2)
+        arr[k++] = rightArr[j++];
+}
+
+// Merge Sort function
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
 }
 
 int main()
@@ -87,8 +128,11 @@ int main()
     // bubble_sort(arr, n);
     // cout << " bubble Sorted array: \n";
     // print_array(arr, n);
-    insertion_sort(arr, n);
-    cout << " insertion Sorted array: \n";
+    // insertion_sort(arr, n);
+    // cout << " insertion Sorted array: \n";
+    // print_array(arr, n);
+    mergeSort(arr, 0, n - 1);
+    cout << " merge Sorted array: \n";
     print_array(arr, n);
     return 0;
 }
